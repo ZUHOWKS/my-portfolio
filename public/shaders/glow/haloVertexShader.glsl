3a90e89670ed1eb367f5430349b1ae52505d1f6a
ZUHOWKS <jojo.vilard38@gmail.com>
2024-01-29 16:30:30 +0100

    Shaders glow effect !depreciate

--- public/shaders/glow/haloVertexShader.glsl
+++ public/shaders/glow/haloVertexShader.glsl
@@ -1,7 +1,11 @@
 export default /* glsl */ `
 
+varying vec2 vUv;
+
 void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0);
+    vUv = uv;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
 
 `;
