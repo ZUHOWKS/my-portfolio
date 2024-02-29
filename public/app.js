0a9f457236a4be99d2c0ea938e6d4b91c3c9dbbd
ZUHOWKS <jojo.vilard38@gmail.com>
2024-02-29 12:15:40 +0100

    Fix web app crash on iOS & Android

--- public/app.js
+++ public/app.js
@@ -12,8 +12,8 @@ const params = {
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / (window.innerHeight-100), 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.physicallyCorrectLights = true;
+const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize( window.innerWidth, window.innerHeight);
 document.querySelector("#holo-sphere").appendChild( renderer.domElement );
 
